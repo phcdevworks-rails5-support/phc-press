bf9d570925fa7f2d4da6c23a7d120beb9cefec69
Brad Potts <bradley.j.potts@gmail.com>
2016-02-19 21:48:58 +0000

    Add ruby version and gemset info.

---
+++ spec/test_app/db/schema.rb
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160219213313) do
+
+  create_table "phcpress_blog_posts", force: :cascade do |t|
+    t.string   "blogpsttitle"
+    t.text     "blogpsttext"
+    t.integer  "pststatus_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "phcpress_blog_posts", ["pststatus_id"], name: "index_phcpress_blog_posts_on_pststatus_id"
+
+  create_table "phcpress_news_posts", force: :cascade do |t|
+    t.string   "newspsttitle"
+    t.text     "newspsttext"
+    t.integer  "pststatus_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "phcpress_news_posts", ["pststatus_id"], name: "index_phcpress_news_posts_on_pststatus_id"
+
+  create_table "phcpress_postprocess_statuses", force: :cascade do |t|
+    t.string   "pststatus"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
