da205df2b14000a8de4eca5047c5011672ea32d9
Brad Potts <bradley.j.potts@gmail.com>
2016-02-20 16:32:15 +0000

    Removed test migrations to write tests.

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220042748) do
+ActiveRecord::Schema.define(version: 20160219213313) do
 
   create_table "phcpress_blog_posts", force: :cascade do |t|
     t.string   "blogpsttitle"
