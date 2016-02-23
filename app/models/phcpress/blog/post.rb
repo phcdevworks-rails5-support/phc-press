0a302408cd10a63ea2a811668232c239c2fd9f61
Brad Potts <bradley.j.potts@gmail.com>
2016-02-23 21:56:16 +0000

    Fixes to image pathes etc.

--- app/models/phcpress/blog/post.rb
+++ app/models/phcpress/blog/post.rb
@@ -1,6 +1,28 @@
 module Phcpress
 	class Blog::Post < ActiveRecord::Base
-		has_attached_file :pstimage, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+		# Declare Attachment
+		has_attached_file :pstimage, 
+		
+		# Set Image Sizes
+		styles: { 
+			largesquare: "520x500>",
+			largerectangle:	"1040x500>",
+			mediumsquare: "360x340>",
+			mediumrectangle: "720x340>",
+			thumb: "150x150>" },
+		
+		# Set Image Local Path
+		:path => ":rails_root/public/images/:id/:style/:filename",
+
+		# Set Image URL Path
+		:url => "/images/:id/:style/:filename",
+
+		# Todo: Change
+		default_url: "/images/:style/missing.png"
+
+		# Validate Images Only
 		validates_attachment_content_type :pstimage, content_type: /\Aimage\/.*\Z/
+
 	end
 end
