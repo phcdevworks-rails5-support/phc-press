7029baf14caf4b0f0c48007df186027101d9e412
Brad Potts <bradley.j.potts@gmail.com>
2016-02-24 00:34:36 +0000

    Updated to 9.5b with minor fixes.

--- app/models/phcpress/news/post.rb
+++ app/models/phcpress/news/post.rb
@@ -1,7 +1,6 @@
 module Phcpress
 	class News::Post < ActiveRecord::Base
 
-		attr_accessible :pstimage
 		mount_uploader :pstimage, Phcpress::PstimageUploader
 
 	end
--- lib/phcpress/version.rb
+++ lib/phcpress/version.rb
@@ -1,3 +1,3 @@
 module Phcpress
-	VERSION = "0.9.5"
+	VERSION = "0.9.5b"
 end
