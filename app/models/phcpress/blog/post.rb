ceef34eedff50dfc79e13dc0ee59b9c02abbb572
Brad Potts <bradley.j.potts@gmail.com>
2016-03-16 22:44:47 +0000

    Add models and layouts.

--- app/models/phcpress/blog/post.rb
+++ app/models/phcpress/blog/post.rb
@@ -1,4 +1,16 @@
 module Phcpress
-  class Blog::Post < ActiveRecord::Base
-  end
+	class Blog::Post < ActiveRecord::Base
+
+		# Define scoped_to for Devise
+		def scoped_to(account)
+			where(:user_id => current_user.id)
+		end
+
+		# For Image Uploads
+		mount_uploader :pstimage, Phcpress::PstimageUploader
+
+		# Associate Post Authour to User
+		belongs_to :user
+
+	end
 end
--- app/models/phcpress/news/post.rb
+++ app/models/phcpress/news/post.rb
@@ -1,4 +1,16 @@
 module Phcpress
-  class News::Post < ActiveRecord::Base
-  end
+	class News::Post < ActiveRecord::Base
+
+		# Define scoped_to for Devise
+		def scoped_to(account)
+			where(:user_id => current_user.id)
+		end
+
+		# For Image Uploads
+		mount_uploader :pstimage, Phcpress::PstimageUploader
+		
+		# Associate Post Authour to User
+		belongs_to :user
+
+	end
 end
