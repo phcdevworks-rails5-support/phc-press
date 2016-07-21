9d87f2a5adda6df5981a0f91cf94107e0902167c
Brad Potts <developers@phcnetworks.net>
2016-07-21 01:13:19 +0000

    add through connections and fixes to models.

--- app/models/phcpress/articles/post.rb
+++ app/models/phcpress/articles/post.rb
@@ -5,7 +5,8 @@ module Phcpress
 		mount_uploader :pstimage, Phcpress::PstimageUploader
 		
 		# Relationships
-		has_many :categories, class_name: 'Modules::Category'
+		has_many :connections, class_name: 'Modules::Connections'
+		has_many :categories, class_name: 'Modules::Category', :through => :connections
 
 	end
 end
--- app/models/phcpress/modules/category.rb
+++ app/models/phcpress/modules/category.rb
@@ -2,7 +2,8 @@ module Phcpress
 	class Modules::Category < ApplicationRecord
 
 		# Relationships
-		has_many :posts, class_name: 'Modules::Category'
+		has_many :connections, class_name: 'Modules::Connections'
+		has_many :posts, class_name: 'Modules::Category', :through => :connections
 
 	end
 end
