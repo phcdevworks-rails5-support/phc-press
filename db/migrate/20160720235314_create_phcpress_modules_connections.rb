b6f424db0d2bb93a60e8093a151115ce8ca9c609
Brad Potts <developers@phcnetworks.net>
2016-07-23 20:50:25 +0000

    adjustments to articles views.

--- db/migrate/20160720235314_create_phcpress_modules_connections.rb
+++ db/migrate/20160720235314_create_phcpress_modules_connections.rb
@@ -1,10 +1,12 @@
 class CreatePhcpressModulesConnections < ActiveRecord::Migration[5.0]
-  def change
-    create_table :phcpress_modules_connections do |t|
-      t.references :post, foreign_key: true
-      t.references :category, foreign_key: true
+	def change
+		create_table :phcpress_modules_connections do |t|
 
-      t.timestamps
-    end
-  end
+			t.references :post
+			t.references :category
+
+			t.timestamps
+
+		end
+	end
 end
