59fb1f3fb48fd3ba936cb60c08b977c4ec6293ba
Brad Potts <developers@phcnetworks.net>
2016-11-14 21:56:34 +0000

    updated navigation.

--- app/helpers/phcpress/application_helper.rb
+++ app/helpers/phcpress/application_helper.rb
@@ -1,4 +1,14 @@
 module Phcpress
 	module ApplicationHelper
+
+		# Add Active to Menu
+		def is_active_controller(controller_name)
+			params[:controller] == controller_name ? "active open selected" : nil
+		end
+
+		def is_active_action(action_name)
+			params[:action] == action_name ? "active" : nil
+		end
+
 	end
 end
--- app/helpers/phcpress/modules/connections_helper.rb
+++
@@ -1,4 +0,0 @@
-module Phcpress
-  module Modules::ConnectionsHelper
-  end
-end
