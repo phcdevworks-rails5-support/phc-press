9bfae885a5de7e94a0de5e60c500c521ce17e16e
Brad Potts <developers@phcnetworks.net>
2016-11-25 17:54:03 +0000

    updated controller add frontend layout.

--- app/controllers/phcpress/frontend/articles_controller.rb
+++ app/controllers/phcpress/frontend/articles_controller.rb
@@ -3,6 +3,9 @@ require_dependency "phcpress/application_controller"
 module Phcpress
 	class Frontend::ArticlesController < ApplicationController
 
+		# Layouts and Filters
+		layout "phcpress/frontend"
+
 		# Article Index
 		def index
 			@frontend_articles = Articles::Post.all
